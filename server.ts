70ddcc4d1669f65dda151637d1e1227ef9bcba09
aho4ahoaho <aho4ahoaho@outlook.jp>
2023-11-18 17:43:26 +0900

    Add Express server and template files

---
+++ server.ts
@@ -0,0 +1,20 @@
+import Express from "express";
+import fs from "fs";
+
+const app = Express();
+
+const tranpiler = new Bun.Transpiler({
+  loader: "ts",
+});
+app.get("/script.js", async (req, res) => {
+  const tscode = fs.readFileSync(__dirname + "/index.ts", "utf-8");
+  const jscode = await tranpiler.transform(tscode);
+  res.setHeader("Content-Type", "application/javascript");
+  res.send(jscode);
+});
+
+app.use(Express.static(__dirname + "/template"));
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
